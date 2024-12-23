1e6b23923d6ffee3a7dd6c0298bc88a770b1e207
saritareddy765 <saritareddy765@gmail.com>
2024-12-23 12:12:11 +0530

    Waits

--- src/test/java/Seleniumexamples/Waits.java
+++ src/test/java/Seleniumexamples/Waits.java
@@ -1,18 +1,18 @@
 package Seleniumexamples;
 
 import org.jspecify.annotations.Nullable;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.devtools.v128.systeminfo.model.Size;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
 public class Waits extends BaseSelenium{
@@ -36,7 +36,7 @@ public class Waits extends BaseSelenium{
         Assert.assertTrue(addToBagButton.isEnabled());
     }
     @Test
-    public void usageOfwebDriverWait() {
+    public void usageOfwebDriverWait()  {
         driver.get("https://www.prettylittlething.com/dark-grey-marl-knit-distressed-detail-chunky-cable-jumper.html");
         driver.manage().window().maximize();
 
@@ -50,4 +50,44 @@ public class Waits extends BaseSelenium{
         System.out.println("addToBagButton :: "+ addToBagButton.isEnabled());
         Assert.assertTrue(addToBagButton.isEnabled());
         }
-    }
+        @Test
+        public void usageOfFluentWait() {
+            driver.get("https://www.prettylittlething.com/dark-grey-marl-knit-distressed-detail-chunky-cable-jumper.html");
+            driver.manage().window().maximize();
+
+            //Explicit wait or conditional wait
+            FluentWait fluentWait = new FluentWait(driver);
+            fluentWait.withTimeout(Duration.ofSeconds(10));
+            fluentWait.pollingEvery(Duration.ofSeconds(2));
+            fluentWait.withMessage("Element not found");
+            fluentWait.ignoring(NoSuchElementException.class);
+            fluentWait.ignoring(ElementClickInterceptedException.class);
+            fluentWait.ignoring(StaleElementReferenceException.class);
+
+
+            FluentWait fluentWait1 = new FluentWait(driver)
+                    .withTimeout(Duration.ofSeconds(10))
+                    .pollingEvery(Duration.ofSeconds(2))
+                    .withMessage("Element not found")
+                    .ignoring(NoSuchElementException.class)
+                    .ignoring(TimeoutException.class);
+
+
+            fluentWait1.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Accept All']")));
+            driver.findElement(By.xpath("//button[text()='Accept All']")).click();
+
+            // driver.findElement(By.xpath("//button[text()='Accept All']")).click();
+            WebElement addToBagButton = (WebElement) fluentWait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("add-to-bag-button"))));
+            boolean getStatusOdAddToBagButton = addToBagButton.isEnabled();
+            System.out.println("getStatusOfAddToBagButton :: " + getStatusOdAddToBagButton);
+
+            WebElement element = (WebElement) fluentWait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath("//span[text()='L']"))));
+//        WebElement size = driver.findElement(By.xpath("//span[text()='M']"));
+//        size.click();
+            element.click();
+
+            System.out.println("addToBagButton status :: " + addToBagButton.isEnabled());
+            Assert.assertTrue(addToBagButton.isEnabled());
+        }
+
+}
