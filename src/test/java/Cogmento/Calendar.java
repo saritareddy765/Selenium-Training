aedca7bcac4bafd91842189e121e36caaed3c005
saritareddy765 <saritareddy765@gmail.com>
2025-06-15 16:18:41 +0530

    cogmento calendar

--- src/test/java/Cogmento/Calendar.java
+++ src/test/java/Cogmento/Calendar.java
@@ -17,16 +17,19 @@ public class Calendar {
         driver.get("https://ui.cogmento.com/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        // login details
         WebElement email = driver.findElement(By.xpath("//input[@name='email']"));
         email.sendKeys("saritareddy3024@gmail.com");
         WebElement password = driver.findElement(By.xpath("//input[@name='password']"));
         password.sendKeys("sarita123");
         WebElement loginButton = driver.findElement(By.xpath("//div[text()='Login']"));
         loginButton.click();
+        //calendar module
         WebElement calendarButton = driver.findElement(By.xpath("//span[contains(text(),'Calendar')]"));
         calendarButton.click();
         WebElement createButton = driver.findElement(By.xpath("//button[@class='ui linkedin button']/i[@class='edit icon']"));
         createButton.click();
+        //details of creating calendar
         WebElement Title = driver.findElement(By.cssSelector("[name='title']"));
         Title.sendKeys("Meeting");
         WebElement Calendar = driver.findElement(By.xpath("//div[@name='calendar']/i"));
